a0f83aa715e7477f18dd5711353d58e97e7bdf77
uapv2102630 <simon.diaz@alumni.univ-avignon.fr>
2024-10-17 10:51:28 +0200

    Fin de l'implémentation des test du TP3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,4 +1,33 @@
 package fr.univavignon.pokedex.api;
 
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class IPokemonTrainerFactoryTest {
+    private IPokemonTrainerFactory pokemonTrainerFactory;
+    private IPokedexFactory pokedexFactory;
+    PokemonTrainer simon ;
+
+    @Before
+    public void setUp() {
+        pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
+        pokedexFactory = mock(IPokedexFactory.class);
+        IPokemonMetadataProvider metadataProvider = mock(IPokemonMetadataProvider.class);
+        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
+        simon  = new PokemonTrainer("Simon",Team.INSTINCT,pokedexFactory.createPokedex(metadataProvider, pokemonFactory));
+    }
+
+    @Test
+    public void testCreatePokemonTrainer(){
+        when(pokemonTrainerFactory.createTrainer("Simon",Team.INSTINCT,pokedexFactory)).thenReturn(simon);
+
+        assertEquals(simon,pokemonTrainerFactory.createTrainer("Simon",Team.INSTINCT,pokedexFactory));
+
+    }
+
 }
